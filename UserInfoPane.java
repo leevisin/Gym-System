08e6104b03113722b81aa531cdf7ee245c0dc909
Crixia1 <1252351765@qq.com>
2021-05-17 22:54:00 +0800

    5/17 22;53

---
+++ UserInfoPane.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.net.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+public class UserInfoPane extends Interface{
+    public JPanel userInfoPane;
+    public JPanel userInfoPanel(){
+        //initialize the Jpanel, set the layout to be null
+        userInfoPane = new JPanel();
+        userInfoPane.setLayout(null);
+
+        
+
+
+
+
+        return userInfoPane;
+        
+    }
+    
+}
