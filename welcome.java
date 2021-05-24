3c1ad999de3fbfe8857df050a33cc1682365229c
MaGe-math150 <83027252+MaGe-math150@users.noreply.github.com>
2021-05-24 13:05:57 +0800

    2
    2

--- welcome.java
+++ welcome.java
@@ -24,9 +24,15 @@ import java.lang.Math;
 
 
 public class welcome extends Interface{
-    public JPanel adImage;
-    public JPanel trainerRC;
-    public JPanel videoRC;
+    public JPanel adImage = new JPanel();
+    public JPanel recommend1 = new JPanel(new BorderLayout());
+    public JPanel recommend2 = new JPanel();
+    public JPanel trainerRC = new JPanel(new BorderLayout());
+    
+    
+    public JPanel recommend3 = new JPanel(new BorderLayout());
+    public JPanel recommend4 = new JPanel();
+    public JPanel videoRC = new JPanel();
     
     
     
@@ -39,7 +45,10 @@ public class welcome extends Interface{
     }
     public JPanel trainerRecommend(){
         //写一个label
-
+        JLabel RC = new JLabel("Trainer recommendation!");
+        RC.setForeground(Color.WHITE);
+        recommend1.add(RC,BorderLayout.WEST);
+        trainerRC.add(recommend1, BorderLayout.NORTH);
         //数组存三个随机数
         int end = readLine("texts/AllTrainer.txt");
         System.out.println("一共有x行"+end);
@@ -47,13 +56,14 @@ public class welcome extends Interface{
         randomInt[0]=getRandom(0, end-1);
         do{
             randomInt[1]= getRandom(0, end-1);
-        }while(!(randomInt[1]==randomInt[0]));
+        }while(randomInt[1]==randomInt[0]);
 
         do{
             randomInt[2]= getRandom(0, end-1);
-        }while(!(randomInt[2]==randomInt[1]||randomInt[2]==randomInt[0]));
+        }while(randomInt[2]==randomInt[1]||randomInt[2]==randomInt[0]);
         
         System.out.println(randomInt[0]+","+randomInt[1]+","+randomInt[2]);
+        
         //先从文件中把图片读出来for循环把加入
         String[][] trainersInfo = readFromFile("texts/AllTrainer.txt");
         
@@ -84,19 +94,38 @@ public class welcome extends Interface{
             trainerBtn.setFocusPainted(false);
             trainerBtn.setVerticalTextPosition(JButton.BOTTOM);
             trainerBtn.setHorizontalTextPosition(JButton.CENTER);
-            trainerRC.add(trainerBtn);
+            recommend2.add(trainerBtn);
             }
+            
+            
+            trainerRC.add(recommend2,BorderLayout.CENTER);
         }
-
-
-
-
         return trainerRC; 
     }
 
     
     
     public JPanel videoRecommend(){
+        public JPanel recommend3 = new JPanel(new BorderLayout());
+        JLabel RC1 = new JLabel("Trainer recommendation!");
+        RC1.setForeground(Color.WHITE);
+        recommend3.add(RC1,BorderLayout.WEST);
+        videoRC.add(recommend1, BorderLayout.NORTH);
+        //数组存三个随机数
+        int end = readLine("texts/AllTrainer.txt");
+        System.out.println("一共有x行"+end);
+        int[] randomInt = new int[3];
+        randomInt[0]=getRandom(0, end-1);
+        do{
+            randomInt[1]= getRandom(0, end-1);
+        }while(randomInt[1]==randomInt[0]);
+
+        do{
+            randomInt[2]= getRandom(0, end-1);
+        }while(randomInt[2]==randomInt[1]||randomInt[2]==randomInt[0]);
+        
+        System.out.println(randomInt[0]+","+randomInt[1]+","+randomInt[2]);
+
         return videoRC;
 
 
